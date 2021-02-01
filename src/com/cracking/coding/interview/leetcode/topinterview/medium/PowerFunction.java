451b270a07dd864e1f27a26910f0574dfc19f87a
nitinFareye1 <nitin.khulbe@getfareye.com>
2021-02-01 08:49:54 +0530

    Power function using recursion

---
+++ src/com/cracking/coding/interview/leetcode/topinterview/medium/PowerFunction.java
@@ -0,0 +1,32 @@
+package com.cracking.coding.interview.leetcode.topinterview.medium;
+
+/**
+ * https://leetcode.com/problems/powx-n/
+ * Medium
+ * Solved using recursion
+ * and simple trick
+ * X -> x/2 * x/2
+ * log(n) complexity
+ */
+public class PowerFunction {
+
+    public double myPow(double x, int n) {
+        if (n < 0) {
+            return 1 / getPow(x, n);
+        }
+        return getPow(x, n);
+    }
+
+    private double getPow(double x, int n) {
+
+        if (n == 0) {
+            return 1;
+        }
+
+        double powerResult = getPow(x, n / 2);
+        if (n % 2 == 0) {
+            return powerResult * powerResult;
+        } else
+            return x * powerResult * powerResult;
+    }
+}
