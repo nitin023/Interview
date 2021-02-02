59d952919eaf78e22efb1ee4050c03a4c70e2813
nitinFareye1 <nitin.khulbe@getfareye.com>
2021-02-02 08:22:24 +0530

    Range sum bst problem

---
+++ src/com/cracking/coding/interview/leetcode/topinterview/recursion/easy/RangeSumInBST.java
@@ -0,0 +1,39 @@
+package com.cracking.coding.interview.leetcode.topinterview.recursion.easy;
+
+import com.cracking.coding.interview.TreesAndGraphs.TreeNode;
+
+/**
+ * Easy
+ * https://leetcode.com/problems/range-sum-of-bst/
+ */
+public class RangeSumInBST {
+    int sum = 0;
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        helper(root, low, high);
+        return sum;
+    }
+
+    private void helper(TreeNode root, int low, int high) {
+        if (root == null) {
+            return;
+        }
+        if (root.val >= low && root.val <= high) {
+            sum += root.val;
+        }
+
+        if (high >= root.val) {
+            helper(root.right, low, high);
+        }
+
+        if (low <= root.val) {
+            helper(root.left, low, high);
+        }
+    }
+
+}
