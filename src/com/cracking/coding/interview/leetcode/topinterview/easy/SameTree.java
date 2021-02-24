1c7983590f39678347ea79992b2dd188caba321f
nitin023 <khulbe.nitin@gmail.com>
2021-02-24 07:24:03 +0530

    Binary tree same tree validation

---
+++ src/com/cracking/coding/interview/leetcode/topinterview/easy/SameTree.java
@@ -0,0 +1,25 @@
+package com.cracking.coding.interview.leetcode.topinterview.easy;
+
+import com.cracking.coding.interview.TreesAndGraphs.TreeNode;
+
+/**
+ * Easy
+ * https://leetcode.com/problems/same-tree/
+ */
+public class SameTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
