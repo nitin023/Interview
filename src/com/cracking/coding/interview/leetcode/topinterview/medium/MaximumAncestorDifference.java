dbf159ef120b99eead35e9ae083544d833b63c23
nitin023 <khulbe.nitin@gmail.com>
2021-02-26 09:17:21 +0530

    Maximum Ancestor Difference

---
+++ src/com/cracking/coding/interview/leetcode/topinterview/medium/MaximumAncestorDifference.java
@@ -0,0 +1,20 @@
+package com.cracking.coding.interview.leetcode.topinterview.medium;
+
+import com.cracking.coding.interview.TreesAndGraphs.TreeNode;
+
+/**
+ * https://leetcode.com/problems/maximum-difference-between-node-and-ancestor
+ * Medium
+ */
+public class MaximumAncestorDifference {
+    public int maxAncestorDiff(TreeNode root) {
+        return dfs(root, root.val, root.val);
+    }
+
+    public int dfs(TreeNode root, int mn, int mx) {
+        if (root == null) return mx - mn;
+        mx = Math.max(mx, root.val);
+        mn = Math.min(mn, root.val);
+        return Math.max(dfs(root.left, mn, mx), dfs(root.right, mn, mx));
+    }
+}
