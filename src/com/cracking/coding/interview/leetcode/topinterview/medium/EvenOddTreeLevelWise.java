76db75b8bd8a69d6c2d43854327363471ebe95be
nitin023 <khulbe.nitin@gmail.com>
2021-02-28 21:40:29 +0530

    Even odd tree level traversal

---
+++ src/com/cracking/coding/interview/leetcode/topinterview/medium/EvenOddTreeLevelWise.java
@@ -0,0 +1,42 @@
+package com.cracking.coding.interview.leetcode.topinterview.medium;
+
+import com.cracking.coding.interview.TreesAndGraphs.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Medium
+ * https://leetcode.com/problems/even-odd-tree/
+ */
+public class EvenOddTreeLevelWise {
+    public boolean isEvenOddTree(TreeNode root) {
+        if (root == null) return true;
+
+        Queue<TreeNode> q = new LinkedList();
+        q.add(root);
+
+        boolean even = true;
+
+        while (q.size() > 0) {
+            int size = q.size();
+
+            int prevVal = even ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+
+            while (size-- > 0) {
+                root = q.poll();
+                if (even && (root.val % 2 == 0 || root.val <= prevVal)) return false;
+
+                if (!even && (root.val % 2 == 1 || root.val >= prevVal)) return false;
+
+                prevVal = root.val;
+
+                if (root.left != null) q.add(root.left);
+                if (root.right != null) q.add(root.right);
+            }
+            even = !even;
+        }
+
+        return true;
+    }
+}
